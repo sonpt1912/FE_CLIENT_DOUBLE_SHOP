41c91f854ad7c6189be4b971661795574101b365
SotaTek-TuanBui3 <tuan.bui3@sotatek.com>
2024-03-21 22:18:34 +0700

    Feat: Display client

--- src/page/Shop.js
+++ src/page/Shop.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Select } from "antd";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <>
+      <div className="search__product">
+        <Select></Select>
+      </div>
+      <div className="product"></div>
+    </>
+  );
 };
 
 export default Shop;
