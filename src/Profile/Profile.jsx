4489fd937ead8d25c46eec7e2dffd94eefeb42e7
thanhhhph28937 <126442063+thanhhhph28937@users.noreply.github.com>
2024-05-09 21:51:51 +0700

    fix

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -3,21 +3,18 @@ import PropTypes from 'prop-types';
 import './Profile.css'
 import avt from './avt.jpg'
 import User from '../API/User';
-import { addSession } from '../Redux/Action/ActionSession';
 import { useDispatch } from 'react-redux';
-<<<<<<< HEAD
-import { Table, Tabs, Button, Modal, Input, Select, Form, Popconfirm, message } from 'antd';
+
+import { Table, Tabs, Button, Modal, Input, Select, Form, Popconfirm, message,Row,Pagination,Col } from 'antd';
 import TabPane from "antd/es/tabs/TabPane";
 import axios from "axios";
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
-=======
-import { Table, Tabs, Button, Modal, Input, Select, Form, Row, Col, Pagination } from 'antd';
-import TabPane from "antd/es/tabs/TabPane";
-import axios from "axios";
-import { SearchOutlined } from '@ant-design/icons';
->>>>>>> efa4d46bce48228cf5c967783a197372c3f87ff3
+import Column from 'antd/es/table/Column';
 
-const { Column } = Table;
+
+import { SearchOutlined } from '@ant-design/icons';
+
+
 const { Search } = Input;
 Profile.propTypes = {
 
